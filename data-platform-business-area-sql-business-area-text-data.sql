dc8ec2f3ac042882b0c59da3919531ed309e7f05
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-12 23:39:09 +0900

    release

--- data-platform-business-area-sql-business-area-text-data.sql
+++ data-platform-business-area-sql-business-area-text-data.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `data_platform_business_area_business_area_text_data`
 (
-  `BusinessArea`          varchar(3) NOT NULL,
-  `Language`              varchar(5) NOT NULL,
-  `BusinessAreaName`      varchar(5) DEFAULT NULL,
+  `BusinessArea`          varchar(4) NOT NULL,
+  `Language`              varchar(3) NOT NULL,
+  `BusinessAreaName`      varchar(100) DEFAULT NULL,
     PRIMARY KEY (`BusinessArea`, `Language`),
     CONSTRAINT `DataPlatformBusinessAreaBusinessAreaTextData_fk` FOREIGN KEY (`BusinessArea`) REFERENCES `data_platform_business_area_business_area_data` (`BusinessArea`)
 ) ENGINE = InnoDB
